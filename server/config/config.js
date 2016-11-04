ff7d9e282d12fca47fde843a8a0022b6a4a4859a
Frédéric Renken <renken@users.noreply.github.com>
2016-11-04 15:04:25 -0700

    deleted empty files, sampledata, and added spoonacular api key

--- server/config/config.js
+++ server/config/config.js
@@ -2,4 +2,5 @@ module.exports = {
   APP_ID: '343b9ac5',
   APP_KEY: '0660e0259a8f4171efb4cc2de3713fdc',
   secret: 'hackreactor',
+  X_Mashape_Key: 'Po2QaBu8uCmshaEonvyWaX5UWQpJp1NshGfjsnV6aF3gnnfMJ5',
 };
