3ff7d34930e5d83caa6c852f5ae04a41cb8f94c0
Matthew Vincent <matthewlawrencevincent@gmail.com>
2016-11-05 13:45:43 -0700

    reworked search bar into ios-style

--- mobile/app/Searchbar.js
+++ mobile/app/Searchbar.js
@@ -1,48 +1,115 @@
 import React from 'react';
+import dismissKeyboard from 'react-native-dismiss-keyboard';
 import {
   StyleSheet,
   TextInput,
   Dimensions,
+  View,
+  Text,
+  LayoutAnimation,
+  TouchableHighlight
 } from 'react-native';
+import styleVariables from '../styleVariables';
 
+const { orange } = styleVariables;
 const width = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   searchBar: {
-    alignItems: 'center',
-    borderWidth: 2,
-    borderColor: 'rgba(0,0,0,.05)',
-    width: width * 0.9,
-    margin: 20,
-    borderRadius: 2,
-    paddingLeft: 10,
-    height: 50,
+    flex: 1,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    height: 30,
+    textAlign: 'center'
   },
+  searchBarFocused: {
+    textAlign: 'left',
+    flex: 4,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    height: 30,
+    maxWidth: width - 80,
+    paddingLeft: 10,
+  },
+  searchBarWrapper: {
+    width: width,
+    maxHeight: 48,
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: orange,
+    flexDirection: 'row',
+    position: 'relative',
+  },
+  cancelButton: {
+    height: 30, 
+    opacity: 1, 
+    width: 60,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+    right: 5,
+    position: 'absolute'
+  }
 });
 
 export default class Searchbar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    this.state = { 
+      text: '' ,
+      focused: false
+    };
   }
 
   reset() {
     this.setState({ text: '' });
   }
 
+  toggleFocus(state) {
+    LayoutAnimation.spring();
+    this.setState({
+      focused: state
+    });
+  }
+
   render() {
     return (
-      <TextInput
-        style={styles.searchBar}
-        underlineColorAndroid="transparent"
-        onChangeText={text => this.setState({ text })}
-        placeholder="Search..."
-        onSubmitEditing={() => {
-          this.props.enter(this.state.text);
-          this.reset();
-        }}
-        returnKeyType={'search'}
-      />
+      <View style={styles.searchBarWrapper}>
+        <TextInput
+          style={this.state.focused 
+            ? styles.searchBarFocused 
+            : styles.searchBar}
+          underlineColorAndroid="transparent"
+          value={this.state.text}
+          onChangeText={text => this.setState({ text })}
+          placeholder="Search Meals..."
+          onSubmitEditing={() => {
+            this.props.enter(this.state.text);
+            this.reset();
+          }}
+          onFocus={() => this.toggleFocus(true)}
+          onBlur={()  => {
+            this.toggleFocus(false)
+            this.reset()
+          }}
+          returnKeyType={'search'}
+          clearButtonMode={'while-editing'}
+          blurOnSubmit={true}
+        />
+        <TouchableHighlight
+          onPress={() => dismissKeyboard() }
+          style={this.state.focused 
+            ? styles.cancelButton 
+            : {width: 0, opacity: 0, height: 30}}
+        >
+          <Text style={{color: 'white'}}>
+            Cancel
+          </Text>
+        </TouchableHighlight>
+      </View>
     );
   }
 }
