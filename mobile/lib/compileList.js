d2681b7442375a60e1c51e53e01e991a57e939cc
Jace <chesszhu@gmail.com>
2016-12-30 15:38:40 -0800

    change shopping list compiler due to edimon API changes

--- mobile/lib/compileList.js
+++ mobile/lib/compileList.js
@@ -2,30 +2,28 @@ const compileList = (meals) => {
   const result = {};
   const list = [];
   const recipes = meals.map(meal => meal.recipe);
-  console.log('ingredients', recipes[0].ingredients[0]);
 
   recipes.forEach((recipe) => {
     recipe.ingredients.forEach((ingredient) => {
-      if (ingredient.food in result) {
-        result[ingredient.food].quantity += ingredient.quantity;
-      } else {
-        result[ingredient.food] = {
-          quantity: ingredient.quantity,
-          measure: ingredient.measure,
-        };
-      }
+      // if (ingredient.food in result) {
+      //   result[ingredient.food].quantity += ingredient.quantity;
+      // } else {
+      //   result[ingredient.food] = {
+      //     quantity: ingredient.quantity,
+      //     measure: ingredient.measure,
+      //   };
+      // }
+      list.push({ ingredient: ingredient.text, quantity: '', checked: false });
     });
   });
 
-  console.log('result', result);
-
-  Object.entries(result).forEach(([ingredient, amount]) => {
-    list.push({
-      ingredient: ingredient,
-      quantity: `${amount.quantity} ${amount.measure}`,
-      checked: false,
-    });
-  });
+  // Object.entries(result).forEach(([ingredient, amount]) => {
+  //   list.push({
+  //     ingredient: ingredient,
+  //     quantity: `${amount.quantity} ${amount.measure}`,
+  //     checked: false,
+  //   });
+  // });
   return list;
 };
 
